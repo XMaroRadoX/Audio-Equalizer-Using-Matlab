f1f4b2e2219ec8c3b494707bba2d8052e21cd312
XMaroRadoX <marwanradwan88@gmail.com>
2022-05-24 17:39:42 +0200

    Skeleton Navigation Done

--- Script.m
+++ Script.m
@@ -47,6 +47,38 @@ if X == 4
 X = listdlg('ListString',list);
 end
 if X == 5 
+    list2 = {'PLAYBACK SPEED (FS)','TYPE OF FILTER','GAIN',...                   
+'RETURN TO DEFAULT','BACK TO PLAYER'};
+Y = listdlg('ListString',list2);
+while Y ~= 5
+    if Y == 1
+        %change fs
+    end
+     if Y == 2
+          list3 = {'FFR','IIR','BACK TO EQUALIZER'};
+        Z = listdlg('ListString',list3);
+        while Z ~= 3
+            if Z == 1
+                %change to ffr
+            end
+            if Z == 2
+                %change to IRR
+            end
+         list2 = {'PLAYBACK SPEED (FS)','TYPE OF FILTER','GAIN',...                   
+'RETURN TO DEFAULT','BACK TO PLAYER'};
+Y = listdlg('ListString',list2);
+     end
+     if Y == 3
+         %change gain
+     end
+     if Y == 4
+        list = {'LOAD','PLAY','PAUSE',...                   
+        'CLEAR','EQUALIZER','EXIT'};
+        X = listdlg('ListString',list);
+    end
+     end
+end
+    
        list = {'LOAD','PLAY','PAUSE',...                   
 'CLEAR','EQUALIZER','EXIT'};
 X = listdlg('ListString',list);
